d9b9067766f1000ca3a27089d8566a192c3acb1d
Sagar Dutta <duttasagar086@gmail.com>
2023-06-09 19:10:27 +0530

    Update models.go

--- models/models.go
+++ models/models.go
@@ -50,10 +50,10 @@ type Address struct {
 type Order struct {
 	Order_ID       primitive.ObjectID `bson:"_id"`
 	Order_Cart     []ProductUser      `json:"order_cart" bson:"order_cart"`
-	Ordered_At     time.Time          `json:"order_at"`
-	Price          int                `json:"price"`
-	Discount       *int               `json:"discount"`
-	Payment_Method Payment            `json:"payment_method"`
+	Ordered_At     time.Time          `json:"order_at" bson:"order_at"`
+	Price          int                `json:"price" bson:"price"`
+	Discount       *int               `json:"discount" bson:"discount"`
+	Payment_Method Payment            `json:"payment_method" bson:"payment_method"`
 }
 
 type Payment struct {
