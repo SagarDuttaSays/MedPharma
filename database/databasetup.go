d2cbed65854fa458ce82c84b6bb4f6838ae1ec18
Sagar Dutta <duttasagar086@gmail.com>
2023-06-10 18:10:40 +0530

    Update databasetup.go

--- database/databasetup.go
+++ database/databasetup.go
@@ -35,7 +35,8 @@ func DBSet() *mongo.Client {
 var Client *mongo.Client = DBSet()
 
 func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
-
+	var collection *mongo.Collection = client.Database("MedPharma").Collection(collectionName)
+	return collection
 }
 
 func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
