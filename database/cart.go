2dbe65123fb3b5110180ff599a65b4fcc252b0dc
Sagar Dutta <duttasagar086@gmail.com>
2023-06-10 17:21:57 +0530

    file updates

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,29 @@
 package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("can't find the product")
+	ErrCantDecodeProducts = errors.New("can't find the product")
+	ErrUserIdIsNotValid   = errors.New("this user isn't valid")
+	ErrCantUpdateUser     = errors.New("can't add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("can't remove this item from the cart")
+	ErrCantGetItem        = errors.New("was unable to get the items from the cart")
+	ErrCantBuyCartItem    = errors.New("can't update the purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
